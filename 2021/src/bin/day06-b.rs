465340624ba5bfce1c5f0e6baa0b56fb12606cd6
David Aurelio <158859+davidaurelio@users.noreply.github.com>
2021-12-14 19:59:14 +0100

    [2021] Day 6, Par 2. Also: cleverer

--- 2021/src/bin/day06-b.rs
+++ 2021/src/bin/day06-b.rs
@@ -5,8 +5,8 @@ use adventofcode_2021::day06::{count_fish, read_fish};
 fn main() -> io::Result<()> {
   let stdin = io::stdin();
   println!(
-    "Produced {} fish in 80 days",
-    count_fish(read_fish(stdin.lock())?, 80)
+    "Produced {} fish in 256 days",
+    count_fish(read_fish(stdin.lock())?, 256)
   );
 
   Ok(())
--- 2021/src/day06.rs
+++ 2021/src/day06.rs
@@ -1,17 +1,22 @@
 use std::io;
 use std::io::BufRead;
 
-pub fn read_fish(mut stdin: io::StdinLock, days: i32) -> io::Result<Vec<i32>> {
+type FishMap = [usize; 9];
+
+pub fn read_fish(mut stdin: io::StdinLock) -> io::Result<FishMap> {
+  let mut map = [0; 9];
   let mut line = String::new();
   stdin.read_line(&mut line)?;
+
   line
     .trim()
     .split(',')
-    .map(|x| parse_num(x).map(|n| days - n))
-    .collect()
+    .map(parse_num)
+    .try_for_each(|x| x.map(|n| map[n] += 1))?;
+  Ok(map)
 }
 
-fn parse_num(x: &str) -> io::Result<i32> {
+fn parse_num(x: &str) -> io::Result<usize> {
   x.parse().map_err(|_| bad_input(x))
 }
 
@@ -19,14 +24,14 @@ fn bad_input(input: &str) -> io::Error {
   io::Error::new(io::ErrorKind::InvalidInput, input)
 }
 
-pub fn count_fish(mut fish: Vec<i32>) -> usize {
-  let mut count = 0usize;
-  while let Some(mut f) = fish.pop() {
-    count += 1;
-    while f > 0 {
-      fish.push(f - 9);
-      f -= 7;
-    }
+pub fn count_fish(mut fish: FishMap, mut days: usize) -> usize {
+  while days > 0 {
+    let zeros = fish[0];
+    fish.copy_within(1.., 0);
+    fish[8] = zeros;
+    fish[6] += zeros;
+    days -= 1;
   }
-  count
+
+  fish.iter().sum()
 }
